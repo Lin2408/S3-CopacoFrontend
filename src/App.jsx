6e41c9ae88c63bbc86f9a3161d6fd535646d70c3
Mazilu, Abel A.R. <i528498@fhict.nl>
2024-10-28 01:52:11 +0100

    Made the item details page prettier and also made it fetch from the back-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import {LoginButton, LogoutButton} from "./Components/AuthButtons.jsx";
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { LoginButton, LogoutButton } from "./Components/AuthButtons.jsx";
 import AuthProviderWithHistory from "./Components/AuthProvider.jsx";
 import Profile from "./Components/Profile.jsx";
-import {getProtectedResource, getPublicResource} from "./Apis/message.service.js";
-import {useAuth0} from "@auth0/auth0-react";
-import {ThemeProvider} from "@mui/material";
-import {theme} from "./Theme.jsx";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./Theme.jsx";
+import ItemDetailsPage from "./Pages/ItemDetailsPage.jsx";
+import HomePage from "./Pages/HomePage.jsx";
 
 function App() {
-    const [count, setCount] = useState(0)
-
+    const [count, setCount] = useState(0);
 
     return (
         <ThemeProvider theme={theme}>
             <AuthProviderWithHistory>
-                <div className="card">
-                    <LoginButton/> <LogoutButton/>
-                </div>
-                <div>
-                    <Profile/>
-                </div>
+                <Router>
+                    <div className="card">
+                        <LoginButton /> <LogoutButton />
+                    </div>
+                    <div>
+                        <Profile />
+                    </div>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/items/:id" element={<ItemDetailsPage />} />
+                    </Routes>
+                </Router>
             </AuthProviderWithHistory>
         </ThemeProvider>
-    )
+    );
 }
 
-export default App
+export default App;
