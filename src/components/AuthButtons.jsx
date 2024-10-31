fce72475f88dc247b5e8d30a961f6b58abbc714d
Timothy Thijsen <504835@student.fontys.nl>
2024-10-31 10:26:06 +0100

    Fixed authbutton having incorrect colors

--- src/components/AuthButtons.jsx
+++ src/components/AuthButtons.jsx
@@ -1,17 +1,18 @@
 import React from "react"
 import { useAuth0 } from "@auth0/auth0-react"
 import {Button} from "@mui/material";
+import "../Navbar.css"
 
 export const LoginButton = () => {
     const {loginWithRedirect} = useAuth0();
     return(
-        <Button variant="contained" color="secondary.light" onClick={() => loginWithRedirect()}>Log In</Button>
+        <Button className="authButton" variant="contained" color="secondary.light" onClick={() => loginWithRedirect()}>Log In</Button>
     )
 }
 
 export const LogoutButton = () => {
     const {logout} = useAuth0();
     return(
-        <Button variant="contained" color="primary" onClick={() => logout({ returnTo: window.location.origin })}>Log Out</Button>
+        <Button className="authButton" variant="contained" color="primary" onClick={() => logout({ returnTo: window.location.origin })}>Log Out</Button>
     )
 }
