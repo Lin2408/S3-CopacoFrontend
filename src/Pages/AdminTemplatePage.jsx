2659b7a33da4d587845b5e80c9db14d6618886e6
Mazilu, Abel A.R. <i528498@fhict.nl>
2024-11-04 14:03:48 +0100

    Saved it

--- src/Pages/AdminTemplatePage.jsx
+++ src/Pages/AdminTemplatePage.jsx
@@ -1,28 +1,29 @@
 import { useState, useEffect } from 'react';
-import { Box, Typography, TextField, Button, Divider, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Box, Typography, TextField, Button, Divider, Select, MenuItem, FormControl, InputLabel, Checkbox, FormControlLabel } from '@mui/material';
 import { getItemsFromCategories } from '/src/Apis/get-items-from-category.service.js';
 import './AdminTemplatePage.css';
 
 const AdminTemplatePage = () => {
     const [templateName, setTemplateName] = useState('');
-    const [price, setPrice] = useState('');
-    const [selectedItems, setSelectedItems] = useState({
-        CPU: '',
-        GPU: '',
-        Motherboard: '',
-        RAM: '',
-    });
-    const [items, setItems] = useState({
-        CPU: [],
-        GPU: [],
-        Motherboard: [],
-        RAM: [],
+    const [selectedItems, setSelectedItems] = useState({});
+    const [items, setItems] = useState({});
+    const [selectedCategories, setSelectedCategories] = useState({
+        CPU: false,
+        Motherboard: false,
+        GPU: false,
+        RAM: false,
     });
 
     useEffect(() => {
         const fetchItems = async () => {
             const fetchedItems = await getItemsFromCategories();
             setItems(fetchedItems);
+            setSelectedItems({
+                CPU: '',
+                Motherboard: '',
+                GPU: '',
+                RAM: '',
+            });
         };
 
         fetchItems();
@@ -32,13 +33,15 @@ const AdminTemplatePage = () => {
         setSelectedItems({ ...selectedItems, [category]: value });
     };
 
+    const handleCategoryToggle = (category) => {
+        setSelectedCategories({ ...selectedCategories, [category]: !selectedCategories[category] });
+    };
+
     const handleSubmit = () => {
-        const newTemplate = { templateName, price, selectedItems };
+        const newTemplate = { templateName, selectedItems };
         console.log("Template Created:", newTemplate);
         // Send template to backend
     };
-
-    const categories = Object.keys(selectedItems);
 
     return (
         <Box sx={{ maxWidth: 600, margin: '40px auto', p: 4, bgcolor: 'white', borderRadius: 2, boxShadow: 3 }}>
@@ -53,31 +56,37 @@ const AdminTemplatePage = () => {
                 sx={{ mb: 3 }}
             />
 
-            <TextField
-                label="Price (€)"
-                variant="outlined"
-                fullWidth
-                type="number"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-                sx={{ mb: 3 }}
-            />
+            <Typography variant="h6" sx={{ mb: 2 }}>Select Categories</Typography>
+            {Object.keys(selectedCategories).map((category) => (
+                <FormControlLabel
+                    key={category}
+                    control={
+                        <Checkbox
+                            checked={selectedCategories[category]}
+                            onChange={() => handleCategoryToggle(category)}
+                            color="primary"
+                        />
+                    }
+                    label={category}
+                />
+            ))}
 
-            <Typography variant="h6" sx={{ mb: 2 }}>Select Components</Typography>
-            {categories.map((category) => (
-                <FormControl fullWidth variant="outlined" sx={{ mb: 3 }} key={category}>
-                    <InputLabel>{category}</InputLabel>
-                    <Select
-                        value={selectedItems[category]}
-                        onChange={(e) => handleSelectChange(category, e.target.value)}
-                        label={category}
-                    >
-                        <MenuItem value=""><em>None</em></MenuItem>
-                        {items[category].map((item) => (
-                            <MenuItem key={item.id} value={item.name}>{item.name}</MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
+            {Object.keys(selectedCategories).map((category) => (
+                selectedCategories[category] && (
+                    <FormControl fullWidth variant="outlined" sx={{ mb: 3 }} key={category}>
+                        <InputLabel>{category}</InputLabel>
+                        <Select
+                            value={selectedItems[category]}
+                            onChange={(e) => handleSelectChange(category, e.target.value)}
+                            label={category}
+                        >
+                            <MenuItem value=""><em>None</em></MenuItem>
+                            {items[category]?.map((item) => (
+                                <MenuItem key={item.id} value={item.name}>{item.name}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                )
             ))}
 
             <Divider sx={{ my: 3 }} />
