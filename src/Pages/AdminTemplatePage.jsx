e5babc7e87b21ea43a77a75da0d594dfc72fc068
Mazilu, Abel A.R. <i528498@fhict.nl>
2024-11-25 13:38:20 +0100

    bugfixed template post

--- src/Pages/AdminTemplatePage.jsx
+++ src/Pages/AdminTemplatePage.jsx
@@ -5,7 +5,7 @@ import { createTemplate } from '/src/Apis/create-template.service.js';
 import './AdminTemplatePage.css';
 
 const AdminTemplatePage = () => {
-    const [templateName, setTemplateName] = useState('');
+    const [name, setTemplateName] = useState('');
     const [selectedCategories, setSelectedCategories] = useState([]);
     const [inputValue, setInputValue] = useState('');
     const [categoryOptions, setCategoryOptions] = useState([]);
@@ -38,8 +38,8 @@ const AdminTemplatePage = () => {
 
     const handleSubmit = async () => {
         const newTemplate = {
-            templateName,
-            selectedCategories: selectedCategories.map(category => category.id)
+            name,
+            categories: selectedCategories.map(category => category.id)
         };
         try {
             await createTemplate(newTemplate);
@@ -57,7 +57,7 @@ const AdminTemplatePage = () => {
                 label="Template Name"
                 variant="outlined"
                 fullWidth
-                value={templateName}
+                value={name}
                 onChange={(e) => setTemplateName(e.target.value)}
                 sx={{ mb: 3 }}
             />
